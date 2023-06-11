20389e7ca3737e24ad87aef3362876bc2e4a7bee
Robert Taylor <rjt903@gmail.com>
2023-06-11 16:17:55 +0100

    Added HRD page placeholder

---
+++ app/homebrew/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import NavBar from "@/components/NavBar";
+import React from "react";
+
+export default function homebrew() {
+    return(
+        <div>
+            <NavBar></NavBar>
+            <h1>HRD</h1>
+        </div>
+    )
+}
