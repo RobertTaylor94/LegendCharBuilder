5a4d84435870d38ec36180b5a90d64b14d775372
Robert Taylor <rjt903@gmail.com>
2023-06-12 21:40:16 +0100

    Added homebrew compilation html and iframe to display on homebrew doc page

--- app/homebrew/page.tsx
+++ app/homebrew/page.tsx
@@ -3,11 +3,14 @@
 import NavBar from "@/components/NavBar";
 import React from "react";
 
+// const myHTML = require('../../components/hrd.html');
+
 export default function homebrew() {
     return(
         <div>
             <NavBar></NavBar>
-            <h1>HRD</h1>
+            {/* <div dangerouslySetInnerHTML={{ __html: myHTML }}></div> */}
+            <iframe src={'hrd.html'} style={{width: '100%', height: '95vh'}}></iframe>
         </div>
     )
 }
