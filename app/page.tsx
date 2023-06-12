1a7daa70929a6a8773ef293c21c444625bf23571
Robert Taylor <rjt903@gmail.com>
2023-06-12 18:51:59 +0100

    Added hero banner to home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,16 @@
 
 import * as React from "react"
 import NavBar from "@/components/NavBar"
+import Image from "next/image"
 
 export default function Home() {
   return(
     <div>
       <NavBar></NavBar>
-      <h1>Hello</h1>
+      <div style={{width: '100vw', height: '50vh', overflow: 'hidden', position: "relative"}}>
+        {/* <img src={"./../public/legend_banner.png"} alt="A hero stood on the edge of a mountain" style={{width: '100vw', height: '50vh'}}/> */}
+        <Image src={"/../public/legend_banner.png"} fill={true} alt="The silouette of a hero standing in a cloudy mountain range. "/>
+      </div>   
     </div>
   )
 };
