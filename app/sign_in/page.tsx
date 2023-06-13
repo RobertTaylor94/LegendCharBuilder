86864845589096e5bc715a93c12b25c9522363da
Robert Taylor <rjt903@gmail.com>
2023-06-13 09:52:27 +0100

    Placeholder page for log in

---
+++ app/sign_in/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import NavBar from "@/components/NavBar";
+import React from "react";
+
+export default function characters() {
+    return(
+        <div>
+            <NavBar></NavBar>
+            <h1>Sign In</h1>
+        </div>
+    )
+}
