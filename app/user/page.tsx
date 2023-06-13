4e3afad115707a9934e83bbbfdbd55e9878a4a22
Robert Taylor <rjt903@gmail.com>
2023-06-13 11:36:22 +0100

    Use context to get logged in user and display profile or signin page

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,9 +1,33 @@
 "use client"
 
 import NavBar from "@/components/NavBar";
+import SignIn from "@/components/auth/SignIn";
 import React from "react";
+import { useAppContext}  from "../../context/state"
+import SignUp from "@/components/auth/SignUp";
 
 export default function characters() {
+
+    let user = useAppContext()
+    console.log("user:")
+    console.log(user)
+
+    if (user == undefined) {
+        return (
+            <div>
+                <NavBar/>
+                <SignUp/>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <NavBar />
+                <SignIn />
+            </div>
+        )
+    }
+
     return(
         <div>
             <NavBar></NavBar>
