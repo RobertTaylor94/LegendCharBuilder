2ba1f16b6608126adad1a6ab9ff6ea0899503f40
Robert Taylor <rjt903@gmail.com>
2023-06-15 23:46:51 +0100

    User profile for Sheet view

---
+++ components/auth/Profile.tsx
@@ -0,0 +1,46 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+    SheetClose,
+    SheetContent,
+    SheetDescription,
+    SheetFooter,
+    SheetHeader,
+    SheetTitle,
+} from "@/components/ui/sheet"
+import { auth } from "../../firebase/config"
+import { signOut } from "firebase/auth"
+
+export default function Profile() {
+
+    const signout = () => {
+        signOut(auth)
+        .then(() => {
+            console.log("sign out succesfull")
+        }).catch((err) => {
+            console.log(err.code)
+            console.log(err.message)
+        })
+    }
+
+    return (
+            <SheetContent position="right" size="default" className="bg-orange-950" style={{ color: 'white' }}>
+                <SheetHeader>
+                    <SheetTitle style={{ color: 'white' }}>Profile</SheetTitle>
+                    <SheetDescription style={{ color: 'white' }}>
+                        User profile.
+                    </SheetDescription>
+                </SheetHeader>
+                <div>
+                    {auth.currentUser?.email}
+                    <Button onClick={signOut}> Sign Out</Button>
+                </div>
+                <SheetFooter>
+                    <SheetClose asChild>
+                        <Button type="submit">Close</Button>
+                    </SheetClose>
+                </SheetFooter>
+            </SheetContent>
+    )
+}
