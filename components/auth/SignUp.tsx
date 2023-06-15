6c72849ca9bdfa45bf47802400c28048381cc43e
Robert Taylor <rjt903@gmail.com>
2023-06-15 22:27:46 +0100

    Added function to handle input and click to create new user with email and password

--- components/auth/SignUp.tsx
+++ components/auth/SignUp.tsx
@@ -11,10 +11,37 @@ import {
     SheetTitle,
     SheetTrigger,
 } from "@/components/ui/sheet"
+import { auth } from "../../firebase/config"
+import { createUserWithEmailAndPassword } from "firebase/auth"
+import { useState } from "react"
+
+type Data = {
+    email: string,
+    password: string
+}
 
 export default function SignUp(props: { handleClick: any; }) {
 
     const {handleClick} = props;
+    const [data, setData] = useState<Data>({email: '', password: ''})
+    const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+        let newInput = { [event.target.name] : event.target.value }
+        setData({...data, ...newInput})
+        console.log(data)
+    }
+    const handleSubmit = (event: React.FormEvent<HTMLButtonElement>) => {
+        createUserWithEmailAndPassword(auth, data.email, data.password)
+        .then((response) => {
+            console.log(response)
+            console.log("User created")
+        })
+        .catch((err) => {
+            console.log(err.message)
+        })
+        const clearData = {email: '', password: ''}
+        setData(clearData)
+    }
+
     return (
             <SheetContent position="right" size="default" className="bg-orange-950" style={{ color: 'white' }}>
                 <SheetHeader>
@@ -28,15 +55,15 @@ export default function SignUp(props: { handleClick: any; }) {
                         <Label htmlFor="name" className="text-right">
                             Username
                         </Label>
-                        <Input id="email" placeholder="Email" className="col-span-3" style={{ backgroundColor: 'white' }} />
+                        <Input id="email" placeholder="Email" className="col-span-3" style={{ backgroundColor: 'white', color: 'black' }} name="email" onChange={handleInput}/>
                     </div>
                     <div className="grid grid-cols-4 items-center gap-4">
                         <Label htmlFor="username" className="text-right">
                             Password
                         </Label>
-                        <Input id="password" placeholder="Password" className="col-span-3" style={{ backgroundColor: 'white' }} />
+                        <Input id="password" placeholder="Password" className="col-span-3" type="password" style={{ backgroundColor: 'white', color: 'black' }} name="password" onChange={handleInput}/>
                     </div>
-                    <Button type='submit' style={{ backgroundColor: '#F8E094', color: '#421407' }}>Save</Button>
+                    <Button type='submit' style={{ backgroundColor: '#F8E094', color: '#421407' }} onClick={handleSubmit}>Sign Up</Button>
                     <div className="grid grid-cols-4 items-center gap-4">
                         <Label className="text-right mr-3 col-span-2">Already signed up?</Label>
                         <Button type='submit' onClick={handleClick} className="col-span-2" style={{ backgroundColor: '#F8E094', color: '#421407' }}>Log In</Button>
